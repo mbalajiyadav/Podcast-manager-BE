510fd292e8d2d13860211bc6109ff5b28333ef60
Mediboyina Balaji <mediboyinabalajiyadav@gmail.com>
2026-05-13 00:05:14 +0530

    Rename AWS keys to S3_ prefix for Vercel

--- controllers/uploadController.js
+++ controllers/uploadController.js
@@ -5,8 +5,8 @@ const multer = require('multer');
 const s3Client = new S3Client({
     region: process.env.AWS_REGION,
     credentials: {
-        accessKeyId: process.env.AWS_ACCESS_KEY,
-        secretAccessKey: process.env.AWS_SECRET_KEY
+        accessKeyId: process.env.S3_ACCESS_KEY,
+        secretAccessKey: process.env.S3_SECRET_KEY
     }
 });
 
