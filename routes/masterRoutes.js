56a0a7fca8f6d2616ff2df8fe179ffd7e335a4dd
Mediboyina Balaji <mediboyinabalajiyadav@gmail.com>
2026-05-12 16:00:21 +0530

    included seeding function

--- routes/masterRoutes.js
+++ routes/masterRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { getRoles, getContentTypes, createContentType, updateContentTypeStatus, getApprovalStatuses } = require('../controllers/masterController');
+const { getRoles, getContentTypes, createContentType, updateContentTypeStatus, getApprovalStatuses, seedMasterData } = require('../controllers/masterController');
 const { protect, authorize } = require('../middleware/authMiddleware');
 
 router.get('/roles', protect, authorize('ADMIN'), getRoles);
@@ -8,5 +8,6 @@ router.get('/content-types', getContentTypes);
 router.post('/content-types', protect, authorize('ADMIN'), createContentType);
 router.patch('/content-types/:id', protect, authorize('ADMIN'), updateContentTypeStatus);
 router.get('/approval-statuses', protect, authorize('ADMIN'), getApprovalStatuses);
+router.get('/seed', seedMasterData);
 
 module.exports = router;
